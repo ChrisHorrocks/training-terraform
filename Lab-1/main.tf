fef4fc5c05335fc8aa1c621255727eca1324c350
Chris Horrocks <chris@Fenris.local>
2023-05-20 10:43:52 +0100

    added Lab-1 final

--- Lab-1/main.tf
+++ Lab-1/main.tf
@@ -1,1 +1,18 @@
-# Terraform Lab-1
+# PROVIDERS
+provider "aws" {
+    region = "eu-west-2"
+}
+
+# RESOURCES
+resource "aws_instance" "test_vm" {
+    ami = "ami-09744628bed84e434"
+
+    instance_type = "t2.micro"
+
+    key_name = "chris_horrocks-london"
+
+    tags = {
+        Name = "Test VM"
+        Owner = "Chris Horrocks"
+    }
+}
