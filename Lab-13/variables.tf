15f24a2723e22b959fd52958c027c0cbb2ec0b81
Chris Horrocks <chris@hor.rocks>
2023-05-25 11:36:55 +0100

    Extended error handling for aws-region var

--- Lab-13/variables.tf
+++ Lab-13/variables.tf
@@ -3,6 +3,6 @@ variable "aws-region" {
     type              = string
     validation {
       condition       = contains(["eu-west-1", "eu-west-2"], var.aws-region)
-      error_message   = "Invalid AWS Region."
+      error_message   = "Invalid or Unapproved AWS Region."
     }
 }
