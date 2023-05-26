b1fce83b69ebef5254df9abb0c5b8b5831f6ee1c
Chris Horrocks <chris@hor.rocks>
2023-05-26 09:54:01 +0100

    final lab-15

---
+++ Lab-15/variables.tf
@@ -0,0 +1,23 @@
+variable "aws-region" {
+    description       = "AWS Region to provision resources into."
+    type              = string
+    validation {
+      condition       = contains(["eu-west-1", "eu-west-2"], var.aws-region)
+      error_message   = "Invalid or Unapproved AWS Region."
+    }
+}
+
+variable "aws-ec2-instance-slug" {
+    description   = "AWS EC2 Instance size."
+    type          = string
+    default       = "t3.micro"
+}
+
+variable "aws-tags-default" {
+    description   = "AWS Tags default set."
+    type          = map(string)
+    default       = {
+      Owner       = "Chris Horrocks"
+      CreatedBy   = "Terraform"
+    }
+}
